7b213e4142152a448075407cdb74ea84b89155bc
ramrodBongTech <wavchild@gmail.com>
2016-11-14 12:52:38 +0000

    First Commit

---
+++ Astar_Threading/Astar_Threading/Astar_Threading.cpp
@@ -0,0 +1,12 @@
+// Astar_Threading.cpp : Defines the entry point for the console application.
+
+#include <SDL.h>
+
+
+int _tmain(int argc, char* argv[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	//SDL_QUIT();
+	return 0;
+}
+
